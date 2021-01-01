6564536aaffd2311eb77286875d0141125db3bcc
GaryZhang <gary.zhangcheng@gmail.com>
2021-01-01 15:32:19 -0600

    Click button

--- XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinHelloWorld
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new GreetPage();
         }
 
         protected override void OnStart()
