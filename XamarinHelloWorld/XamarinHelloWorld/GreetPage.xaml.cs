d39be52f75d9a2918ea77a4e61ec8200efee62e0
GaryZhang <gary.zhangcheng@gmail.com>
2021-01-02 09:39:54 -0600

    Dealing with Device Differences

--- XamarinHelloWorld/XamarinHelloWorld/GreetPage.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/GreetPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +14,36 @@ namespace XamarinHelloWorld
         public GreetPage()
         {
             InitializeComponent();
+
+            //non-generic overload
+            //[Obsolete]
+            //Device.OnPlatform(
+            //    iOS: () =>
+            //    {
+            //        Padding = new Thickness(0, 20, 0, 0);
+            //},
+            //    Android: () => {
+            //        Padding = new Thickness(10, 20, 0, 0);
+            //},
+            //    WinPhone: () => {
+            //        Padding = new Thickness(30, 20, 0, 0);
+            //});
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    this.Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                case Device.Android:
+                    this.Padding = new Thickness(10, 20, 0, 0);
+                    break;
+                case Device.UWP:
+                    this.Padding = new Thickness(20, 20, 0, 0);
+                    break;
+                case Device.macOS:
+                    this.Padding = new Thickness(30, 20, 0, 0);
+                    break;
+            }
         }
     }
 }
