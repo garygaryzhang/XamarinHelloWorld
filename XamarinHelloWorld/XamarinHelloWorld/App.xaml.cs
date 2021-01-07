6c50e2628a37a6664c3ec9babcef88178fff04b4
GaryZhang <gary.zhangcheng@gmail.com>
2021-01-06 21:11:10 -0600

    exercise login page

--- XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinHelloWorld
         {
             InitializeComponent();
 
-            MainPage = new StackPage();
+            MainPage = new StackExercisePage();
         }
 
         protected override void OnStart()
