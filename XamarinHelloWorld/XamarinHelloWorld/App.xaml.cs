6909564a88530e4fb47c3c15cb7f3f82365fe75e
GaryZhang <gary.zhangcheng@gmail.com>
2021-01-12 21:58:29 -0600

    absolute layout in xmal

--- XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinHelloWorld
         {
             InitializeComponent();
 
-            MainPage = new GridLogin();
+            MainPage = new AbsolutePage();
         }
 
         protected override void OnStart()
