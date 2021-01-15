30384e097a3c74baf776d955d8699b0cfaf9dbdb
GaryZhang <gary.zhangcheng@gmail.com>
2021-01-14 22:53:20 -0600

    Done

--- XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinHelloWorld
         {
             InitializeComponent();
 
-            MainPage = new AbsoluteLayoutAdsPage();
+            MainPage = new AbsoluteLayoutMainPage();
         }
 
         protected override void OnStart()
