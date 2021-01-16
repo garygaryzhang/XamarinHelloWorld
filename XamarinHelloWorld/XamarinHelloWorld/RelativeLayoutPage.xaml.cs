4040a9798d150b131beedf92fa389988627666b8
GaryZhang <gary.zhangcheng@gmail.com>
2021-01-15 22:37:46 -0600

    Relative Layout in code

--- XamarinHelloWorld/XamarinHelloWorld/RelativeLayoutPage.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/RelativeLayoutPage.xaml.cs
@@ -15,6 +15,18 @@ namespace XamarinHelloWorld
         public RelativeLayoutPage()
         {
             InitializeComponent();
+
+            //var layout = new RelativeLayout();
+            //Content = layout;
+
+            //var aquaBox = new BoxView { Color = Color.Aqua };
+            //layout.Children.Add(aquaBox,
+            //    widthConstraint: Constraint.RelativeToParent(parent => parent.Width),
+            //    heightConstraint: Constraint.RelativeToParent(parent => parent.Height));
+
+            //var silverBox = new BoxView { Color = Color.Silver };
+            //layout.Children.Add(silverBox,
+            //    yConstraint: Constraint.RelativeToView(aquaBox, (RelativeLayout, element) => element.Height + 20));
         }
     }
 }
