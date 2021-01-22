71d2d16811df452be1de9453190fba54d8ed7c05
GaryZhang <gary.zhangcheng@gmail.com>
2021-01-21 23:05:59 -0600

    Done

--- XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinHelloWorld
         {
             InitializeComponent();
 
-            MainPage = new ImagePage();
+            MainPage = new EmbeddedImages();
         }
 
         protected override void OnStart()
