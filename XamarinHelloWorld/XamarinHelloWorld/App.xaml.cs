8592dd673240f4408f6ab6ddceda1542d2faa90e
GaryZhang <gary.zhangcheng@gmail.com>
2021-02-03 22:06:24 -0600

    done

--- XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinHelloWorld
         {
             InitializeComponent();
 
-            MainPage = new ImageSlideShowPage();
+            MainPage = new SimpleListPage();
 
         }
 
