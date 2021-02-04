c7635f5b067b8c3f52e59cd74534318ae39ff488
GaryZhang <gary.zhangcheng@gmail.com>
2021-02-03 22:06:24 -0600

    done

---
+++ XamarinHelloWorld/XamarinHelloWorld/SimpleListPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinHelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SimpleListPage : ContentPage
+    {
+        public SimpleListPage()
+        {
+            InitializeComponent();
+
+            var fruits = new List<string>
+            {
+                "banana",
+                "apple",
+                "pear"
+            };
+
+            listView.ItemsSource = fruits;
+        }
+    }
+}
