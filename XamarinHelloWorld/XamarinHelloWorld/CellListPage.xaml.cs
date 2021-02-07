72f4f16ae6a974fae8b116662598fa6bfa12ad4c
GaryZhang <gary.zhangcheng@gmail.com>
2021-02-07 16:20:23 -0600

    pull to refresh

--- XamarinHelloWorld/XamarinHelloWorld/CellListPage.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/CellListPage.xaml.cs
@@ -29,6 +29,16 @@ namespace XamarinHelloWorld
             listView.ItemsSource = _contacts;
         }
 
+        ObservableCollection<Contact> GetContacts()
+        {
+            return new ObservableCollection<Contact>
+            {
+                new Contact { Name = "Gary", ImageUrl = "https://picsum.photos/id/237/200", Status = "YOLO"},
+                new Contact { Name = "Kendal", ImageUrl = "https://picsum.photos/236/200", Status = "Hello World!"},
+                new Contact { Name = "Ricca", ImageUrl = "https://picsum.photos/235/200", Status = "Meow World!"}
+            };
+        }
+
         private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             var contact = e.Item as Contact;
@@ -55,5 +65,11 @@ namespace XamarinHelloWorld
             DisplayAlert("Call", contact.Name, "OK");
         }
 
+        private void listView_Refreshing(object sender, EventArgs e)
+        {
+            listView.ItemsSource = GetContacts();
+
+            listView.EndRefresh();
+        }
     }
 }
