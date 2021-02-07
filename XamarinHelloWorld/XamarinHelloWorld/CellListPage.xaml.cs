5bc0eae36e4f551ee3794cd654958b49a22501e4
GaryZhang <gary.zhangcheng@gmail.com>
2021-02-07 15:18:03 -0600

    Tap and select

--- XamarinHelloWorld/XamarinHelloWorld/CellListPage.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/CellListPage.xaml.cs
@@ -17,19 +17,24 @@ namespace XamarinHelloWorld
         {
             InitializeComponent();
 
-            listView.ItemsSource = new List<ContactGroup>
+            listView.ItemsSource = new List<Contact>
             {
-                new ContactGroup("G","G") {
-                    new Contact { Name = "Gary", ImageUrl = "https://picsum.photos/id/237/200", Status = "YOLO"}
-                },
-                new ContactGroup("K","K") {
-                    new Contact { Name = "Kendal", ImageUrl = "https://picsum.photos/236/200", Status = "Hello World!"}
-                },
-                new ContactGroup("R","R") {
+                    new Contact { Name = "Gary", ImageUrl = "https://picsum.photos/id/237/200", Status = "YOLO"},
+                    new Contact { Name = "Kendal", ImageUrl = "https://picsum.photos/236/200", Status = "Hello World!"},
                     new Contact { Name = "Ricca", ImageUrl = "https://picsum.photos/235/200", Status = "Meow World!"}
-                }
             };
+        }
 
+        private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var contact = e.Item as Contact;
+            DisplayAlert("Tapped", contact.Name, "OK");
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var contact = e.SelectedItem as Contact;
+            DisplayAlert("Selected", contact.Name, "OK");
         }
     }
 }
