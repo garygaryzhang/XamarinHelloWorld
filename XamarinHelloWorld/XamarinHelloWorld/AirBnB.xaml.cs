7891cefc7c6711c804adf5e19808c8b97b42d548
GaryZhang <gary.zhangcheng@gmail.com>
2021-02-23 19:42:31 -0600

    wip

---
+++ XamarinHelloWorld/XamarinHelloWorld/AirBnB.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinHelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AirBnB : ContentPage
+    {
+        public AirBnB()
+        {
+            InitializeComponent();
+        }
+
+        private void listView_Refreshing(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
+++ XamarinHelloWorld/XamarinHelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinHelloWorld
         {
             InitializeComponent();
 
-            MainPage = new CellListPage();
+            MainPage = new AirBnB();
 
         }
 
